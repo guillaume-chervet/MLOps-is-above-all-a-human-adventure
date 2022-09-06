d8e72559fdff1f1d6733080536c28554a29633f1
Guillaume Chervet <guillaume.chervet@gmail.com>
2022-09-06 18:11:30 +0200

    first commit

---
+++ index.py
@@ -0,0 +1,168 @@
+from matplotlib import pyplot
+from matplotlib.image import imread
+
+#https://machinelearningmastery.com/how-to-develop-a-convolutional-neural-network-to-classify-photos-of-dogs-and-cats/
+
+# define location of dataset
+folder = 'dogs-vs-cats/train/'
+# plot first few images
+for i in range(9):
+	# define subplot
+	pyplot.subplot(330 + 1 + i)
+	# define filename
+	filename = folder + 'dog.' + str(i) + '.jpg'
+	# load image pixels
+	image = imread(filename)
+	# plot raw pixel data
+	pyplot.imshow(image)
+# show the figure
+pyplot.show()
+
+
+
+# organize dataset into a useful structure
+from os import makedirs
+from os import listdir
+from shutil import copyfile
+from random import seed
+from random import random
+# create directories
+dataset_home = 'dataset_dogs_vs_cats/'
+subdirs = ['train/', 'test/']
+for subdir in subdirs:
+	# create label subdirectories
+	labeldirs = ['dogs/', 'cats/']
+	for labldir in labeldirs:
+		newdir = dataset_home + subdir + labldir
+		makedirs(newdir, exist_ok=True)
+# seed random number generator
+seed(1)
+# define ratio of pictures to use for validation
+val_ratio = 0.25
+number_file_limit = 100
+# copy training dataset images into subdirectories
+src_directory = 'dogs-vs-cats/train/'
+for file in listdir(src_directory):
+
+	src = src_directory + '/' + file
+	dst_dir = 'train/'
+	current_number_file_limit = number_file_limit
+	if random() < val_ratio:
+		dst_dir = 'test/'
+		current_number_file_limit = int(number_file_limit*val_ratio)
+	if file.startswith('cat'):
+		directory = dataset_home + dst_dir + 'cats/'
+		dst = directory + file
+		if len(listdir(directory)) > current_number_file_limit:
+			continue
+		copyfile(src, dst)
+	elif file.startswith('dog'):
+		directory = dataset_home + dst_dir + 'dogs/'
+		dst = directory + file
+		if len(listdir(directory)) > current_number_file_limit:
+			continue
+		copyfile(src, dst)
+
+ 
+# vgg16 model used for transfer learning on the dogs and cats dataset
+import sys
+from matplotlib import pyplot
+from keras.applications.vgg16 import VGG16
+from keras.models import Model
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.optimizers import SGD
+from keras.preprocessing.image import ImageDataGenerator
+
+# define cnn model
+def define_model():
+	# load model
+	model = VGG16(include_top=False, input_shape=(224, 224, 3))
+	# mark loaded layers as not trainable
+	for layer in model.layers:
+		layer.trainable = False
+	# add new classifier layers
+	flat1 = Flatten()(model.layers[-1].output)
+	class1 = Dense(128, activation='relu', kernel_initializer='he_uniform')(flat1)
+	output = Dense(1, activation='sigmoid')(class1)
+	# define new model
+	model = Model(inputs=model.inputs, outputs=output)
+	# compile model
+	opt = SGD(lr=0.001, momentum=0.9)
+	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+	return model
+
+# plot diagnostic learning curves
+def summarize_diagnostics(history):
+	# plot loss
+	pyplot.subplot(211)
+	pyplot.title('Cross Entropy Loss')
+	pyplot.plot(history.history['loss'], color='blue', label='train')
+	pyplot.plot(history.history['val_loss'], color='orange', label='test')
+	# plot accuracy
+	pyplot.subplot(212)
+	pyplot.title('Classification Accuracy')
+	pyplot.plot(history.history['accuracy'], color='blue', label='train')
+	pyplot.plot(history.history['val_accuracy'], color='orange', label='test')
+	# save plot to file
+	filename = sys.argv[0].split('/')[-1]
+	pyplot.savefig(filename + '_plot.png')
+	pyplot.close()
+
+# run the test harness for evaluating a model
+def run_test_harness():
+	# define model
+	model = define_model()
+	# create data generator
+	datagen = ImageDataGenerator(featurewise_center=True)
+	# specify imagenet mean values for centering
+	datagen.mean = [123.68, 116.779, 103.939]
+	# prepare iterator
+	train_it = datagen.flow_from_directory('dataset_dogs_vs_cats/train/',
+		class_mode='binary', batch_size=64, target_size=(224, 224))
+	test_it = datagen.flow_from_directory('dataset_dogs_vs_cats/test/',
+		class_mode='binary', batch_size=64, target_size=(224, 224))
+	# fit model
+	history = model.fit_generator(train_it, steps_per_epoch=len(train_it),
+		validation_data=test_it, validation_steps=len(test_it), epochs=10, verbose=1)
+	# evaluate model
+	_, acc = model.evaluate_generator(test_it, steps=len(test_it), verbose=0)
+	print('> %.3f' % (acc * 100.0))
+	# learning curves
+	summarize_diagnostics(history)
+	model.save('final_model.h5')
+
+# entry point, run the test harness
+run_test_harness()
+
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+
+
+# load and prepare the image
+def load_image(filename):
+	# load the image
+	img = load_img(filename, target_size=(224, 224))
+	# convert to array
+	img = img_to_array(img)
+	# reshape into a single sample with 3 channels
+	img = img.reshape(1, 224, 224, 3)
+	# center pixel data
+	img = img.astype('float32')
+	img = img - [123.68, 116.779, 103.939]
+	return img
+
+
+# load an image and predict the class
+def run_example():
+	# load the image
+	img = load_image('sample_image.jpg')
+	# load model
+	model = load_model('final_model.h5')
+	# predict the class
+	result = model.predict(img)
+	print(result[0])
+
+
+# entry point, run the example
+run_example()
